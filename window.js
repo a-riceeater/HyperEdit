6db8b304534f03f8c0f20c1d85e5f64b13647be9
darthvader1925 <elijb12@gmail.com>
2023-10-07 11:19:36 -0700

    page base

--- window.js
+++ window.js
@@ -1,1 +1,35 @@
-const electron = require("electron")
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            devTools: true
+        },
+        resizable: true,
+        frame: false,
+        contextIsolation: false,
+        enableRemoteModule: true,
+    })
+
+    win.loadFile(path.join(__dirname, "page", "window.html"))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
