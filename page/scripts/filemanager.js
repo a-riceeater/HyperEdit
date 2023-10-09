9673775396caba9e54c43630cad82267e5faae6f
darthvader1925 <elijb12@gmail.com>
2023-10-09 16:59:50 -0700

    requires

--- page/scripts/filemanager.js
+++ page/scripts/filemanager.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-const { ipcRenderer } = require("electron");
-
 async function openFolder() {
     const files = await ipcRenderer.invoke("openFolder");
     console.dir(files);
