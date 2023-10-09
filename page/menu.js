8359826177e84ffc65d2c6d537037bec2d161cc1
darthvader1925 <elijb12@gmail.com>
2023-10-09 16:50:45 -0700

    zindex

--- page/menu.js
+++ page/menu.js
@@ -1,4 +1,7 @@
 if (process.platform == 'darwin') {
     document.querySelector(".topbar-btns").style.display = "none"
     document.querySelector(".topbar > .dft-logo").style.right = "10px"
-} else document.querySelector(".topbar > .dft-logo").style.left = "5px"
+} else {
+    document.querySelector(".topbar > .dft-logo").style.left = "5px"
+    document.querySelector(".topbar-btns").style.display = "block"
+} 
