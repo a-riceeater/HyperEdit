ca49be53dfd4e5521cef623f4c4d7a8c665a10f0
darthvader1925 <elijb12@gmail.com>
2023-10-10 18:32:47 -0700

    tab function

--- page/scripts/tabs.js
+++ page/scripts/tabs.js
@@ -3,7 +3,7 @@ const editorEle = document.querySelector(".editor");
 
 document.querySelectorAll(".tabbar > .tab-btn > span").forEach(el => {
     el.addEventListener("click", (e) => {
-        if (currentTab == e.target.parentNode.getAttribute("id")) {
+        if (currentTab == "tab-" + e.target.parentNode.getAttribute("id")) {
             editorEle.innerHTML = `
             <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
                 <img src="./logo.png" height="200px" width="200px" style="margin-bottom: 0">
@@ -17,7 +17,7 @@ document.querySelectorAll(".tabbar > .tab-btn > span").forEach(el => {
 })
 
 class Tab {
-    constructor (file) {
+    constructor(file) {
         const fdata = fs.readFileSync(file, "utf8");
         let lines = fdata.split("\n").length;
         let linedata = "";
@@ -25,7 +25,33 @@ class Tab {
         for (let i = 0; i < lines; i++) {
             linedata += `${i + 1}<br>`
         }
-        
+
+        document.querySelectorAll(".tab-btn.selected").forEach(e => e.classList.remove("selected"));
+
+        const tbe = document.createElement("div");
+        tbe.innerHTML = `<span>${file.replace(/^.*[\\\/]/, '')}</span>`
+        tbe.classList.add("tab-btn");
+        tbe.classList.add("selected");
+        tbe.id = `tab-${file}`
+
+        tbe.childNodes.forEach(el => {
+            if (el.nodeName != "SPAN") return
+            el.addEventListener("click", () => {
+                if (currentTab == "tab-" + file) {
+                    editorEle.innerHTML = `
+                    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
+                        <img src="./logo.png" height="200px" width="200px" style="margin-bottom: 0">
+                        <p style="margin-top: -20px; font-family: 'Lato', sans-serif; color: lightgray; user-select: none; cursor: default;">Open a file to begin</p>
+                    </div>
+                    `
+                }
+    
+                tbe.remove();
+            })
+        })
+
+        document.querySelector(".tabbar").appendChild(tbe);
+
         document.querySelector(".editor").innerHTML = `
         <div id="lineNumbers" onscroll="return false">
             ${linedata}
@@ -36,14 +62,14 @@ class Tab {
         <textarea id="codeInput" spellcheck="false" autofocus="true" autocomplete="off">${fdata}</textarea>
         `
 
-        currentTab = file;
+        currentTab = "tab-" + file;
 
         setTimeout(() => {
             document.querySelector("#lineNumbers").addEventListener("scroll", (e) => {
                 document.querySelector("#codeInput").scrollTop = e.target.scrollTop;
                 document.querySelector("#highlight").scrollTop = e.target.scrollTop;
             });
-            
+
             document.querySelector("#codeInput").addEventListener("scroll", (e) => {
                 document.querySelector("#lineNumbers").scrollTop = e.target.scrollTop;
                 document.querySelector("#highlight").scrollTop = e.target.scrollTop;
